c80a8b0042142046b711875fbcb268e14b3e3243
Sébastien Krafft <skrafft@gmail.com>
2019-10-11 11:50:14 +0200

    Fixed timeframe error and added logging

--- server/middleware/sqs-consumer.js
+++ server/middleware/sqs-consumer.js
@@ -32,78 +32,81 @@ module.exports = function(options) {
   	const consumer = Consumer.create({
   	  queueUrl: process.env.SQS_QUEUE_URL,
   	    handleMessage: (message, done) => {
-          if (handler.handleMessage && typeof handler.handleMessage === 'function') {
-            handler.handleMessage(message, done);
+        if (handler.handleMessage && typeof handler.handleMessage === 'function') {
+          handler.handleMessage(message, done);
       	  } else {
-              console.log(message);
-              done();
+          console.log(message);
+          done();
       	  }
-    		}
+    		},
   	});
 
     consumer.on('error', function(err, message) {
-       if (handler.onError && typeof handler.onError === 'function') {
-         handler.onError(err, message);
+      if (handler.onError && typeof handler.onError === 'function') {
+        handler.onError(err, message);
    	  }
     });
 
     consumer.on('processing_error', function(err, message) {
-       if (handler.onProcessingError && typeof handler.onProcessingError === 'function') {
-         handler.onProcessingError(err, message);
+      if (handler.onProcessingError && typeof handler.onProcessingError === 'function') {
+        handler.onProcessingError(err, message);
    	  }
     });
 
     consumer.on('message_received', function(message) {
-       if (handler.onMessageReceived && typeof handler.onMessageReceived === 'function') {
-         handler.onMessageReceived(message);
+      if (handler.onMessageReceived && typeof handler.onMessageReceived === 'function') {
+        handler.onMessageReceived(message);
    	  }
     });
 
     consumer.on('message_processed', function(message) {
-       if (handler.onMessageProcessed && typeof handler.onMessageProcessed === 'function') {
-         handler.onMessageProcessed(message);
+      if (handler.onMessageProcessed && typeof handler.onMessageProcessed === 'function') {
+        handler.onMessageProcessed(message);
    	  }
     });
 
     consumer.on('response_processed', function() {
-       if (handler.onResponseProcessed && typeof handler.onResponseProcessed === 'function') {
-         handler.onResponseProcessed();
+      if (handler.onResponseProcessed && typeof handler.onResponseProcessed === 'function') {
+        handler.onResponseProcessed();
    	  }
     });
 
     consumer.on('stopped', function() {
-       if (handler.onStopped && typeof handler.onStopped === 'function') {
-         handler.onStopped();
+      if (handler.onStopped && typeof handler.onStopped === 'function') {
+        handler.onStopped();
    	  }
     });
 
     consumer.on('empty', function() {
-       if (handler.onEmpty && typeof handler.onEmpty === 'function') {
-         handler.onEmpty();
+      if (handler.onEmpty && typeof handler.onEmpty === 'function') {
+        handler.onEmpty();
    	  }
     });
 
     if (options.schedule && options.schedule.timezone && options.schedule.from && options.schedule.to) {
-      setInterval(() => {
+      const manageConsumer = () => {
         const now = moment().tz(options.schedule.timezone);
         const [fromHour, fromMinutes] = options.schedule.from.split(':');
         const [toHour, toMinutes] = options.schedule.to.split(':');
-        const from = now.startOf('day').hour(Number(fromHour)).minute(Number(fromMinutes));
-        const to = now.startOf('day').hour(Number(toHour)).minute(Number(toMinutes));
-        if (now.isAfter(from) && now.isAfter(to)) {
+        const from = now.clone().startOf('day').hour(Number(fromHour)).minute(Number(fromMinutes));
+        const to = now.clone().startOf('day').hour(Number(toHour)).minute(Number(toMinutes));
+        if (now.isAfter(from) && now.isBefore(to)) {
           if (consumerStatus !== STATUS_STARTED) {
-            console.log('Starting SQS consumer');
+            console.log('In schedule time, starting SQS consumer');
             consumer.start();
             consumerStatus = STATUS_STARTED;
           }
         } else {
           if (consumerStatus !== STATUS_STOPPED) {
-            console.log('Stopping SQS consumer');
+            console.log('Out of schedule time, stopping SQS consumer');
             consumer.stop();
             consumerStatus = STATUS_STOPPED;
           }
         }
-      }, 60);
+      };
+      console.log('Schedule is active. Only consuming data from ' + options.schedule.from + ' to ' + options.schedule.to + ' (' + options.schedule.timezone + ')');
+      manageConsumer();
+      setInterval(manageConsumer, 60000);
     } else {
       consumer.start();
       consumerStatus = STATUS_STARTED;
